dd5b04f3a8e139ebb5a5ed58da6ff01dbbe20220
Eric Karlsson <erickarlsson.ek@gmail.com>
2024-02-29 16:15:01 +0100

    Add project files.

---
+++ WebApp/Models/Views/HomeIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using WebApp.Models.Sections;
+
+namespace WebApp.Models.Views;
+
+public class HomeIndexViewModel
+{
+    public string Title { get; set; } = "Ultimate Task Management Assistant";
+    public ShowcaseViewModel Showcase { get; set; } = new ShowcaseViewModel
+    {
+        Id = "overview",
+        ShowcaseImage = new() { ImageUrl = "/images/showcase-taskmaster.svg", AltText = "Task Managment Assistant", },
+        Title = "Task Management Assistant You Gonna Love",
+        Text = "We offer you a new generation of task management system. Plan, manage & track all your tasks in one flexible tool.\r\n",
+        Link = new() { ControllerName = "Downloads", ActionName = "Index", Text = "Get started for free" },
+        BrandsText = "Largest companies use our tool to work efficiently",
+        Brands =
+                [
+                    new() { ImageUrl = "~images/brands/brand_1.svg", AltText = "Brand Name 1"},
+                    new() { ImageUrl = "/images/brands/brand_2.svg", AltText = "Brand Name 2"},
+                    new() { ImageUrl = "/images/brands/brand_3.svg", AltText = "Brand Name 3"},
+                    new() { ImageUrl = "/images/brands/brand_4.svg", AltText = "Brand Name 4"},
+                ]
+    };
+         
+
+}
