42c1fb1e0811f6fa6e7f63b2afbbadb126bb75e9
Eric Karlsson <erickarlsson.ek@gmail.com>
2024-03-01 15:02:46 +0100

    Signup init

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    [Route("/signup")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+ 
+}
