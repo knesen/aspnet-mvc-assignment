c03cef5d02a349b8a50bd505df1fb73eb080fb2b
Eric Karlsson <erickarlsson.ek@gmail.com>
2024-03-11 15:28:29 +0100

    Migration and DB set up, context init

---
+++ Infrastructure/Repositories/Repo.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Models;
+
+namespace Infrastructure.Repositories;
+
+abstract class Repo<TEntity>(DataContext context) where TEntity : class
+{
+    private readonly DataContext _context = context;
+
+    public virtual async Task<ResponseResult> CreateOneAsync(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return new ResponseResult
+            {
+                ContentResult = entity,
+                Message = "Created successfully",
+                StatusCode = StatusCode.OK,
+            };
+        }
+        catch (Exception ex) 
+        {
+            return new ResponseResult
+            {
+                Message = ex.Message,
+                StatusCode = StatusCode.ERROR,
+            };
+        }
+        
+    }
+}
