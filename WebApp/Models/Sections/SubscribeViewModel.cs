4303e0ef23f81ea98257bdd2bbfb1a777952a361
Eric Karlsson <erickarlsson.ek@gmail.com>
2024-04-21 19:29:12 +0200

    Final fix

--- WebApp/Models/Sections/SubscribeViewModel.cs
+++ WebApp/Models/Sections/SubscribeViewModel.cs
@@ -5,16 +5,15 @@ namespace WebApp.Models.Sections;
 
 public class SubscribeViewModel
 {
-    public string? Id {  get; set; }
+    public string? Id { get; set; }
     public string? Title { get; set; }
     public string? SubTitle { get; set; }
 
     public SubscriberModel Subscriber { get; set; } = null!;
 
     public ImageViewModel? Image { get; set; }
+    public List<ItemListViewModel>? Subscribe { get; set; }
     public string? TermsText { get; set; }
     public List<LinkViewModel>? Terms { get; set; }
-    
-
 
 }
